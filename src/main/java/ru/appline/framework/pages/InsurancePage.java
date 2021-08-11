56a59c7309fcd6a05f0dfcebdb57ac4d95fbf11a
Arkadiy Alaverdyan <aalaverdyan@appline.ru>
2021-08-12 01:05:02 +0300

    Правки для PageObject (Selenium + JUnit + Allure)

--- src/main/java/ru/appline/framework/pages/InsurancePage.java
+++ src/main/java/ru/appline/framework/pages/InsurancePage.java
@@ -1,5 +1,6 @@
 package ru.appline.framework.pages;
 
+import io.qameta.allure.Step;
 import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -22,13 +23,19 @@ public class InsurancePage extends BasePage {
      *
      * @return InsurancePage - т.е. остаемся на этой странице
      */
+    @Step("Проверяем что открылась страница 'Страхование путешественников'")
     public InsurancePage checkOpenInsurancePage() {
         Assert.assertEquals("Заголовок отсутствует/не соответствует требуемому",
                 "Страхование путешественников", title.getText());
         return this;
     }
 
-
+    /**
+     * Кликаем по кнопку оформить онлайн
+     *
+     * @return SelectInsuranceServicePage - т.е. переходим на страницу {@link SelectInsuranceServicePage}
+     */
+    @Step("Кликаем по кнопке 'Оформить онлайн' на странице 'Страхование путешественников'")
     public SelectInsuranceServicePage checkoutOnline() {
         waitUtilElementToBeClickable(buttonCheckoutOnline).click();
         return pageManager.getSelectInsuranceServicePage().checkOpenSelectInsuranceServicePage();
